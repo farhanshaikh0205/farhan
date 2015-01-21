fd082abbdc7262fa12f301af1e73aee312bbd3d4
farhanshaikh0205 <farhanshaikh0205@gmail.com>
2015-01-21 16:34:01 +0530

    code update 4

--- display.php
+++ display.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+        echo "EmpId: " . $row["id"]. " EmpName: " . $row["name"]. "<br>";
     }
 } else {
     echo "0 results";
